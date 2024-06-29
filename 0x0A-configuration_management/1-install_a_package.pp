0c312bcd61725b2079e6190240416a0e7c5ef9a3
mfonisodonatuseffiong <kaytwobaba@gmail.com>
2024-06-29 13:48:27 +0100

    0x0A. Configuration management

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -3,9 +3,10 @@ package { 'python3-pip':
   ensure => installed,
 }
 
-# Install Flask version 2.1.0 using pip3
+# Install Flask version 2.1.0 using pip3 with increased timeout
 package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
+  install_options => ['--timeout=120'],
   require  => Package['python3-pip'],  # Ensure pip3 is installed before Flask
 }
