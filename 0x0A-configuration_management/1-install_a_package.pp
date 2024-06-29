6d2b48d7ba814f49057af84668f8a94fb83b97d7
mfonisodonatuseffiong <kaytwobaba@gmail.com>
2024-06-29 17:28:20 +0100

    0x0A. Configuration management

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -3,9 +3,9 @@ package { 'python3-pip':
   ensure => installed,
 }
 
-# Install Flask version 2.1.0 and compatible Werkzeug version using pip3
+# Install Flask version 2.1.0 and Werkzeug version 2.1.1 using pip3
 exec { 'install_flask_and_werkzeug':
-  command => '/usr/local/bin/pip3 install flask==2.1.0 werkzeug==2.0.3',
+  command => '/usr/bin/pip3 install flask==2.1.0 werkzeug==2.1.1',
   path    => ['/usr/bin', '/usr/local/bin'],
   require => Package['python3-pip'],
 }
