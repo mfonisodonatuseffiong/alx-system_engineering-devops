eee230452ed67af7b972558bb51101c42f57c44e
mfonisodonatuseffiong <kaytwobaba@gmail.com>
2024-07-29 19:42:41 +0100

    0x15. API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -9,6 +9,7 @@ import sys
 
 REST_API = "https://jsonplaceholder.typicode.com"
 
+
 def fetch_data(employee_id):
     """Fetch employee and task data from the API."""
     try:
@@ -25,6 +26,7 @@ def fetch_data(employee_id):
         print(f"Error: {e}")
         sys.exit(1)
 
+
 if __name__ == '__main__':
     if len(sys.argv) > 1 and re.fullmatch(r'\d+', sys.argv[1]):
         employee_id = int(sys.argv[1])
@@ -34,7 +36,8 @@ if __name__ == '__main__':
         tasks = [task for task in todos if task.get('userId') == employee_id]
         completed_tasks = [task for task in tasks if task.get('completed')]
 
-        print(f'Employee {emp_name} is done with tasks({len(completed_tasks)}/{len(tasks)}):')
+        print(f'Employee {emp_name} is done with tasks '
+              f'({len(completed_tasks)}/{len(tasks)}):')
         for task in completed_tasks:
             print(f'\t {task.get("title")}')
     else:
