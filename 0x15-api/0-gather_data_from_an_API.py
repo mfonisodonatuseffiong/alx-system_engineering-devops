3ef410ff4fe825a6fdbeee546720d2f3acb3cb3f
mfonisodonatuseffiong <kaytwobaba@gmail.com>
2024-07-29 14:14:55 +0100

    0x15. API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-'''
-gather employee data from API
-'''
+"""
+Gather employee data from API.
+"""
 
 import re
 import requests
@@ -12,19 +12,14 @@ REST_API = "https://jsonplaceholder.typicode.com"
 if __name__ == '__main__':
     if len(sys.argv) > 1:
         if re.fullmatch(r'\d+', sys.argv[1]):
-            id = int(sys.argv[1])
-            req = requests.get('{}/users/{}'.format(REST_API, id)).json()
-            task_req = requests.get('{}/todos'.format(REST_API)).json()
+            employee_id = int(sys.argv[1])
+            req = requests.get(f'{REST_API}/users/{employee_id}').json()
+            task_req = requests.get(f'{REST_API}/todos').json()
             emp_name = req.get('name')
-            tasks = list(filter(lambda x: x.get('userId') == id, task_req))
-            completed_tasks = list(filter(lambda x: x.get('completed'), tasks))
+            tasks = [task for task in task_req if task.get('userId') == employee_id]
+            completed_tasks = [task for task in tasks if task.get('completed')]
             print(
-                'Employee {} is done with tasks({}/{}):'.format(
-                    emp_name,
-                    len(completed_tasks),
-                    len(tasks)
-                )
+                f'Employee {emp_name} is done with tasks({len(completed_tasks)}/{len(tasks)}):'
             )
-            if len(completed_tasks) > 0:
-                for task in completed_tasks:
-                    print('\t {}'.format(task.get('title')))
+            for task in completed_tasks:
+                print(f'\t {task.get("title")}')
