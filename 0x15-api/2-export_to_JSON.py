758c7d01a15c1ab5bb31712559964fa4feac5e25
mfonisodonatuseffiong <kaytwobaba@gmail.com>
2024-07-29 20:21:10 +0100

    0x15. API

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """ Python to get data from an API and convert to JSON"""
 
-import csv
 import json
 import requests
 import sys
@@ -29,4 +28,3 @@ if __name__ == '__main__':
     """print(dict_data)"""
     with open('{}.json'.format(USER_ID), 'w') as f:
         json.dump(dict_data, f)
-
