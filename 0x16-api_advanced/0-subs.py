9bc4c59717437211e70a354e1c068658cd57016a
mfonisodonatuseffiong <kaytwobaba@gmail.com>
2024-08-14 18:43:28 +0100

    0x16. API advanced

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,24 +1,27 @@
 #!/usr/bin/python3
 """
-This module provides a function to query the Reddit API for
-the number of subscribers of a given subreddit.
+number of subscribers for a given subreddit
 """
 
-import requests
+from requests import get
 
 
 def number_of_subscribers(subreddit):
-    """Queries the Reddit API and returns the number of subscribers
-    to the subreddit"""
-    
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {"User-Agent": "MyRedditApp/0.0.1 (by /u/your_username)"}
+    """
+    function that queries the Reddit API and returns the number of subscribers
+    (not active users, total subscribers) for a given subreddit.
+    """
+
+    if subreddit is None or not isinstance(subreddit, str):
+        return 0
+
+    user_agent = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    response = get(url, headers=user_agent)
+    results = response.json()
 
     try:
-        sub_info = requests.get(url, headers=headers, allow_redirects=False)
-        if sub_info.status_code == 200:
-            data = sub_info.json().get("data", {})
-            return data.get("subscribers", 0)
-        return 0
-    except requests.RequestException:
+        return results.get('data').get('subscribers')
+
+    except Exception:
         return 0
