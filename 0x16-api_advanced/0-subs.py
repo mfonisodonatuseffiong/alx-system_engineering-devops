48c24708d7edc2e2b16cf2e80f1a73375b97add4
mfonisodonatuseffiong <kaytwobaba@gmail.com>
2024-08-16 00:47:52 +0100

    0x16. API advanced

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,12 +8,16 @@ import requests
 
 def number_of_subscribers(subreddit):
     """Return the total number of subscribers on a given subreddit."""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {"User-Agent": "Mozilla/5.0"}
     response = requests.get(url, headers=headers, allow_redirects=False)
+    
     if response.status_code == 200:
-        data = response.json()
-        subscribers = data['data']['subscribers']
-        return subscribers
+        try:
+            data = response.json()
+            subscribers = data['data']['subscribers']
+            return subscribers
+        except KeyError:
+            return 0
     else:
         return 0
