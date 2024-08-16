caa11e13766afbd5e8653647c0bfc8f1f790499c
mfonisodonatuseffiong <kaytwobaba@gmail.com>
2024-08-16 01:42:32 +0100

    0x16. API advanced

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -8,6 +8,7 @@ import requests
 
 def top_ten(subreddit):
     """Print the titles of the 10 hottest posts on a given subreddit."""
+
     # Construct the URL for the subreddit's hot posts in JSON format
     url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
 
@@ -34,4 +35,5 @@ def top_ten(subreddit):
     results = response.json().get("data")
 
     # Print the titles of the top 10 hottest posts
-    [print(c.get("data").get("title")) for c in results.get("children")]
+    for post in results.get("children"):
+        print(post.get("data").get("title"))
